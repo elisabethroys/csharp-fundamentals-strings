8994793736c23c98d693411b969c7d24e30ca631
Elisabeth Røysland <elisabeth.skartveit@lyse.net>
2025-08-08 15:49:59 +0200

    All tests passed, Elisabeth Røysland

--- csharp-fundamentals-strings.Main/Core.cs
+++ csharp-fundamentals-strings.Main/Core.cs
@@ -14,7 +14,7 @@ namespace csharp_fundamentals_strings.Main
         //TODO: 1. The brokenUrl member above contains an invalid URL. There's a z instead of an s in the protocol (httpz instead of https).
         // Using the `replace` method on brokenUrl, set the fixedUrl member below to the correct value.
         // https://learn.microsoft.com/en-us/dotnet/api/system.string.replace?view=net-7.0
-        public string fixedUrl => string.Empty;
+        public string fixedUrl => brokenUrl.Replace('z', 's');
 
 
         // Here's a documentation link for all string methods, use it to figure out how to complete the rest of these requirements:
@@ -23,27 +23,27 @@ namespace csharp_fundamentals_strings.Main
 
         //TODO: 2. There are currently some upper case characters in the URL. Using an appropriate string method on the fixedUrl member above,
         // set the value of lowerCasedUrl.
-        public string lowerCasedUrl => string.Empty;
+        public string lowerCasedUrl => fixedUrl.ToLower();
 
 
         //TODO: 3. There is still white space on both ends of the URL! Use the appropriate string method to trim that white space
         // and set the value of the url member below
-        public string url => string.Empty;
+        public string url => lowerCasedUrl.Trim();
 
 
         //TODO: 4. Using the appropriate string method on url, set the value of the protocol member below
-        public string protocol => string.Empty;
+        public string protocol => url.Split(":")[0];
 
 
         //TODO: 5. Using the appropriate string method on url, set the value of the domain member below
-        public string domain => string.Empty;
+        public string domain => url.Split("/")[2];
 
 
         //TODO: 6. Set the length member below to the length of the url member
-        public int length => 0;
+        public int length => url.Length;
 
 
         //TODO: 7. Using concatenation and existing members, set the faqUrl member below to the faq page of the boolean website
-        public string faqUrl => string.Empty;
+        public string faqUrl => protocol + "://" + domain + "/faq";
     }
 }
--- csharp-fundamentals-strings.Main/Extension.cs
+++ csharp-fundamentals-strings.Main/Extension.cs
@@ -43,7 +43,7 @@ namespace csharp_fundamentals_strings.Main
             // 1. Using the sb variable above, add "Hello, world!" to the StringBuilder
             // WRITE YOUR CODE BETWEEN THIS LINE...
 
-
+            sb.Append("Hello, world!");
 
             // ...AND THIS LINE
 
@@ -58,7 +58,8 @@ namespace csharp_fundamentals_strings.Main
             // 2. After adding the message, use an appropriate StringBuilder method to reverse it
             // WRITE YOUR CODE BETWEEN THIS LINE...
 
-
+            sb.Append("Hello, world!");
+            sb = new StringBuilder(new string(sb.ToString().Reverse().ToArray()));
 
             // ...AND THIS LINE
 
@@ -73,7 +74,8 @@ namespace csharp_fundamentals_strings.Main
             // 2. After adding the message, remove the comma.
             // WRITE YOUR CODE BETWEEN THIS LINE...
 
-
+            sb.Append("Hello, world!");
+            sb.Remove(5, 1);
 
             // ...AND THIS LINE
 
@@ -88,6 +90,8 @@ namespace csharp_fundamentals_strings.Main
             // 2. After adding the message, replace the word "world" with the word "C#"
             // WRITE YOUR CODE BETWEEN THIS LINE...
 
+            sb.Append("Hello, world!");
+            sb.Replace("world", "C#");
 
 
             // ...AND THIS LINE
